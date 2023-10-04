d6f182ed8a85a10d4ef6aae2747a42d620f76473
Steven Llamas <146597349+steven-llamas@users.noreply.github.com>
2023-10-03 19:15:07 -0500

    Update 1_CREATE_USERS.ps1

--- AD_Users_Script/1_CREATE_USERS.ps1
+++ AD_Users_Script/1_CREATE_USERS.ps1
@@ -1,10 +1,10 @@
 ﻿# ----- Edit these Variables for your own Use Case ----- #
-$PASSWORD_FOR_USERS   = "Password!"
-$USER_FIRST_LAST_LIST = Get-Content .\names.txt
+$PASSWORD_FOR_USERS   = "Password!"                # sets Password! as the password for all the users when created.
+$USER_FIRST_LAST_LIST = Get-Content .\names.txt    # places .\names.txt in the variable $USER_FIRST_LAST_LIST.
 # ------------------------------------------------------ #
 
-$password = ConvertTo-SecureString $PASSWORD_FOR_USERS -AsPlainText -Force
-New-ADOrganizationalUnit -Name _USERS -ProtectedFromAccidentalDeletion $false
+$password = ConvertTo-SecureString $PASSWORD_FOR_USERS -AsPlainText -Force  
+New-ADOrganizationalUnit -Name _USERS -ProtectedFromAccidentalDeletion $false   # Created AD organizational unit _USERS and does not protect them from accidential deletion.
 
 foreach ($n in $USER_FIRST_LAST_LIST) {
     $first = $n.Split(" ")[0].ToLower()
@@ -22,3 +22,7 @@ foreach ($n in $USER_FIRST_LAST_LIST) {
                -Path "ou=_USERS,$(([ADSI]`"").distinguishedName)" `
                -Enabled $true
 }
+
+# the code above states for each name in file ($n) it created $first (first name) and $last (last name) by splitting the space into two and puts their name in respective varables.
+# then the username variable is created by taking the first letter (0) and using + the rest of the last name.
+# new-adUser --- creates a new user with the variables for each user.
